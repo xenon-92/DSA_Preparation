10fbcd6a8cd9ad16fff94b88dd75607c77b71ebf
xenon-92 <sumitjana92@hotmail.com>
2020-10-10 12:22:38 +0530

    find value for a given position

--- LinkedList/2ReturnValueForApos/Program.cs
+++ LinkedList/2ReturnValueForApos/Program.cs
@@ -6,7 +6,50 @@ namespace _2ReturnValueForApos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Node head = new Node(1);
+            Node n1 = new Node(11);
+            Node n2 = new Node(111);
+            Node n3 = new Node(1111);
+            Node n4 = new Node(11111);
+            head.next = n1;
+            n1.next = n2;
+            n2.next = n3;
+            n3.next = n4;
+            Solution s = new Solution();
+            int result = s.GetValueAtPosition(head, 5);
+            Console.WriteLine(result);
+        }
+    }
+    class Node
+    {
+        public int value;
+        public Node next;
+        public Node(int val)
+        {
+            this.value = val;
+            this.next = null;
+        }
+    }
+    class Solution
+    {
+        public int GetValueAtPosition(Node head, int pos)
+        {
+            int position = 0;
+            Node current = head;
+            bool flag = false;
+            while (current != null)
+            {
+                if (position == pos)
+                {
+                    flag = true;
+                    break;
+                }
+                current = current.next;
+                position += 1;
+            }
+            if (flag)
+                return current.value;
+            return -1;
         }
     }
 }
